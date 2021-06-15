9605cc0994343ae7f97f37abde885def723439bc
aisaasghari <aisaasghari@gmail.com>
2021-06-15 05:08:34 -0700

    Step 3: Insert Income & Outgo UI & Logic Done, Report UI Done, Drawer Done

--- app/src/main/java/ir/shariaty/hesabche/MainActivity.java
+++ app/src/main/java/ir/shariaty/hesabche/MainActivity.java
@@ -1,15 +1,74 @@
 package ir.shariaty.hesabche;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+import ir.shariaty.hesabche.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    DrawerLayout drawerLayout;
+    ImageView btMenu;
+    RecyclerView recyclerView;
+    static ArrayList<String> arrayList = new ArrayList<>();
+    MainAdapter adapter;
+
+    public static void closeDrawer(DrawerLayout drawerLayout) {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        btMenu = findViewById(R.id.bt_menu);
+        recyclerView = findViewById(R.id.recycler_view);
+
+        arrayList.clear();
+
+        arrayList.add("Income");
+        arrayList.add("Outgo");
+        arrayList.add("Report");
+        arrayList.add("About");
+        arrayList.add("Invite Friends");
+        arrayList.add("Logout");
+
+        adapter = new MainAdapter(this, arrayList);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        recyclerView.setAdapter(adapter);
+
+        btMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //Close Drawer
+        closeDrawer(drawerLayout);
 
     }
 }
