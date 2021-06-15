96515b26a94c70f5538b89ac3741c4cddb5f1f62
aisaasghari <aisaasghari@gmail.com>
2021-06-15 14:20:23 -0700

    Step 4: Invite Friends via SMS UI & Logic Done!

--- app/src/main/java/ir/shariaty/hesabche/InviteActivity.java
+++ app/src/main/java/ir/shariaty/hesabche/InviteActivity.java
@@ -1,14 +1,69 @@
 package ir.shariaty.hesabche;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Toast;
 
-public class InviteActivity extends AppCompatActivity {
+import ir.shariaty.hesabche.databinding.ActivityIntroBinding;
+import ir.shariaty.hesabche.databinding.ActivityInviteBinding;
+
+public class InviteActivity extends DrawerActivity {
+
+    ActivityInviteBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_invite);
+
+        binding=ActivityInviteBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(InviteActivity.this, Manifest.permission.SEND_SMS)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    sendMessage();
+                } else {
+                    ActivityCompat.requestPermissions(InviteActivity.this, new String[]{Manifest.permission.SEND_SMS}, 100);
+                }
+            }
+        });
+
     }
+
+    private void sendMessage() {
+        String sPhone = binding.etPhone.getText().toString().trim();
+        String sMessage = binding.etMessage.getText().toString().trim();
+
+        if (!sPhone.equals("") && !sMessage.equals("")) {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(sPhone, null, sMessage, null, null);
+            Toast.makeText(getApplicationContext(), "Invitation Sent Successfully!", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Fill the Fields!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 100 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            sendMessage();
+        } else {
+            Toast.makeText(getApplicationContext(), "Access Denied!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 }
